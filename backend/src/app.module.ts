b32f604da1d0c2eb7593ce6d44a9b2869cd2b19d
ithiagosoares <ti.santos10@gmail.com>
2026-09-25 20:27:12 -0300

    auth: configuração JwtModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AprendizagemModule } from './aprendizagem/aprendizagem.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AprendizagemModule,UsuariosModule],
+  imports: [AprendizagemModule,UsuariosModule,AuthModule],
 })
 export class AppModule {}
