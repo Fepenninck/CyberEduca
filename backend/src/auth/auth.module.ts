ed8f1b8020169f5a2dcdf7efa104f334e17d1443
Caiomn <caiomiranda174@gmail.com>
2026-09-26 13:47:31 -0300

    fix: remove marcadores de conflito do Git

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,35 +1,17 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-=======
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './jwt.strategy';
-import { AuthController } from './auth.controller';
->>>>>>> 7e0ccd6fa78fcf05f37c1ef0194593bca577f791
 import { PrismaService } from '../prisma.service';
 
 @Module({
   imports: [
-<<<<<<< HEAD
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'chave_secreta_super_segura',
-=======
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'segredo-temporario-dev',
->>>>>>> 7e0ccd6fa78fcf05f37c1ef0194593bca577f791
       signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
-<<<<<<< HEAD
   providers: [AuthService, PrismaService],
-=======
-  providers: [JwtStrategy, PrismaService],
-  exports: [JwtModule, PassportModule],
->>>>>>> 7e0ccd6fa78fcf05f37c1ef0194593bca577f791
 })
 export class AuthModule {}
